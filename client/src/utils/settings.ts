5b78c35e4c38b38e0b50da56036ef128762b4942
hugisveitti <hugiholm1@gmail.com>
2021-09-15 14:53:42 +0200

    tablet

--- client/src/utils/settings.ts
+++ client/src/utils/settings.ts
@@ -1,6 +1,9 @@
 export const getDeviceType = () => {
     const ua = navigator.userAgent;
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        if (window.confirm("Press OK, to use as mobile and cancel to use as desktop")) {
+            return "mobile"
+        }
         return "desktop" // "tablet";
     } else if (
         /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
