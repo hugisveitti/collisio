b2f04843c31c7cf3c99dc2a392bc9cd7458722bb
hugisveitti <hugiholm1@gmail.com>
2021-09-15 14:25:30 +0200

    did not push

--- server.js
+++ server.js
@@ -12,28 +12,23 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static(path.join(__dirname, "./client/src")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/src/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-
 app.get("/wait", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/src/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.get("/game", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/src/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.get("/controls", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/src/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
-app.get("/index.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/src/index.js"));
-})
 
 
 
@@ -41,7 +36,6 @@ const port = process.env.PORT || 5000
 const server = app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
-
 
 
 const GameMaster = require("./one-monitor-game/OneMonitorGame")
