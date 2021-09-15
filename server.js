60a30f5789753e2a253a526666d99009f512acd0
hugisveitti <hugiholm1@gmail.com>
2021-09-15 13:55:35 +0200

    changing port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/controls", (req, res) => {
 
 
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 const server = app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
